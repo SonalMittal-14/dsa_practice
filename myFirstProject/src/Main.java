d2db9f705ab72aa446c437cb7dd1ca4996186761
SonalMittal-14 <sonalmittal140103@gmail.com>
2024-01-11 01:13:48 +0530

    11 jan

---
+++ myFirstProject/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world with java");
+        System.out.println("hello world with java");
+        System.out.println("hello ji \n myself sonal");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
