1b29bf9d2780af46a0844a219e8ff06aedf53348
SonalMittal-14 <sonalmittal140103@gmail.com>
2024-01-11 01:47:07 +0530

    11

---
+++ myFirstProject/Loops.java
@@ -0,0 +1,13 @@
+public class Loops {
+    public static void main(String[] args) {
+        for(int counter=0; counter < 11; counter++){
+            System.out.println(counter);
+        }
+
+    //    int i = 0;
+    //    while (i<11) {
+    //     System.out.println(i);
+    //     i++;
+    //    } 
+    }
+}
---
+++ myFirstProject/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+ for(int counter=0; counter < 11; counter++){
+            System.out.println(counter);
+        }
